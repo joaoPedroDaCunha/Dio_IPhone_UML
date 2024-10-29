5a3195ec811eb0869855e8d23376c0656b91f7a6
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-29 10:19:48 -0300

    create class Playerlist

--- IPhone_java/src/Entity/MusicPlayer/PlayerList.java
+++ IPhone_java/src/Entity/MusicPlayer/PlayerList.java
@@ -1,5 +1,22 @@
 package Entity.MusicPlayer;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PlayerList {
     
+    private Set<MusicFile> playerList;
+
+    public PlayerList(){
+        playerList = new HashSet<>();
+    }
+
+    public void addMusic(String musicName,String musicFilePath){
+        playerList.add(new MusicFile(musicName, musicFilePath));
+    }
+
+    public void removeMusic(Set<MusicFile> removemusic){
+        playerList.removeAll(removemusic);
+    }
+
 }
