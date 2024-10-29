7075fe34b25b89709e36bc8b68810837ddbcfe46
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-29 09:21:25 -0300

    create class musicFile

---
+++ IPhone_java/src/Entity/MusicPlayer/MusicFile.java
@@ -0,0 +1,21 @@
+package Entity.MusicPlayer;
+
+public class MusicFile {
+
+    private String musicFilePath;
+
+    public MusicFile(String musicFilePath) {
+        this.musicFilePath = musicFilePath;
+    }
+
+    public String getMusicFilePath() {
+        return musicFilePath;
+    }
+
+    public void setMusicFilePath(String musicFilePath) {
+        this.musicFilePath = musicFilePath;
+    }
+
+    
+    
+}
