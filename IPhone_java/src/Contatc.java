8ad9a495edd03071c3cec4134bf22ec850f26c55
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-29 08:41:29 -0300

    Create README end Contatc object

---
+++ IPhone_java/src/Contatc.java
@@ -0,0 +1,70 @@
+public class Contatc {
+    private String nameContact;
+    private int numberContact;
+    private String discriptionContact;
+    
+    public Contatc(String nameContact, int numberContact) {
+        this.nameContact = nameContact;
+        this.numberContact = numberContact;
+    }
+
+    public Contatc(String nameContact, int numberContact, String discriptionContact) {
+        this.nameContact = nameContact;
+        this.numberContact = numberContact;
+        this.discriptionContact = discriptionContact;
+    }
+
+    public String getNameContact() {
+        return nameContact;
+    }
+
+    public void setNameContact(String nameContact) {
+        this.nameContact = nameContact;
+    }
+
+    public int getNumberContact() {
+        return numberContact;
+    }
+
+    public void setNumberContact(int numberContact) {
+        this.numberContact = numberContact;
+    }
+
+    public String getDiscriptionContact() {
+        return discriptionContact;
+    }
+
+    public void setDiscriptionContact(String discriptionContact) {
+        this.discriptionContact = discriptionContact;
+    }
+
+    @Override
+    public String toString() {
+        return "[nameContact=" + nameContact + ", numberContact=" + numberContact + ", discriptionContact="
+                + discriptionContact + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + numberContact;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Contatc other = (Contatc) obj;
+        if (numberContact != other.numberContact)
+            return false;
+        return true;
+    }
+
+    
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+## Descrição do Desafio
+Com base no vídeo de lançamento do iPhone conforme link abaixo, elabore em uma ferramenta de UML de sua preferência. A diagramação das classes e interfaces com a proposta de representar os papéis do iPhone de: Reprodutor Musical, Aparelho Telefônico e Navegador na Internet. Em seguida crie as classes e interfaces no formato de arquivos .java
+
+https://www.youtube.com/watch?v=9ou608QQRq8
+
+## Pré-requisitos:
+- Lógica de programação e POO;
+
+- Conhecimentos básicos(Java);
+
+- Computador com SO de sua preferência(Windows, Linux, Mac OS);
+
+- IDE (Visual Studio Code, Eclipse, etc);
